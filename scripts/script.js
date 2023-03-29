c7b93a72ab6b14414867e83cef9e4eae50551ca9
Neda <nedachangizi@gmail.com>
2023-03-29 13:49:42 -0700

    button once alert

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,8 @@ const button = document.querySelector("#clickMe");
 //2. define a function to alert user
 function alertUser() {
   alert("you clicked!");
+//   button.removeEventListener("click", alertUser);
+
 }
 //3. add the function as event listener
-button.addEventListener("click", alertUser);
+button.addEventListener("click", alertUser,{once:true});
